1041af6c0e3cb628906f130c637dbda36c296fac
Nil Carmona <160121117+NilCarmona@users.noreply.github.com>
2024-03-19 15:32:27 +0100

    Update TestReadLine.java

--- P1/NoMVC/TestReadLine.java
+++ P1/NoMVC/TestReadLine.java
@@ -2,7 +2,9 @@ import java.io.*;
 
 class TestReadLine {
   public static void main(String[] args) {
-    System.out.println("Presione Alt+E [EXIT] para resetar el terminal\r");
+    System.out.println("Presione Alt+E para entrar en modo EDIT\r");
+    System.out.println("Estas en modo insert (los caracteres se añaden), Pulsa la tecla insert para salir de este modo (los caracteres se sobreponen)\r");
+    
     BufferedReader in = new EditableBufferedReader(new InputStreamReader(System.in));
     String str = null;
     try {
